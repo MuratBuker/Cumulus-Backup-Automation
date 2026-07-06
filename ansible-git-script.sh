d0b7f422009a0d71e5049141ce8729c86b697b06
MuratBuker <murat_buker@yahoo.com>
2026-07-06 17:05:01 +0300

    changed ansible-git-script.sh

--- ansible-git-script.sh
+++ ansible-git-script.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 ansible-playbook /root/ansible/NVIDIA/nvidia-backup-playbook.yaml -i /root/ansible/NVIDIA/inventory.ini
-cd /root/equinix/
+cd /root/dc1/
 git add -A
-git commit -m "Equinix Push"
+git commit -m "DC 1 Push"
 git push
-cd /root/kkb/
+cd /root/dc2/
 git add -A
-git commit -m "KKB Push"
+git commit -m "DC 2 Push"
 git push
-cd /root/turksat/
+cd /root/dc3/
 git add -A
-git commit -m "Turksat Push"
+git commit -m "DC 3 Push"
 git push
-cd /root/onprem/
+cd /root/dc4/
 git add -A
-git commit -m " OnPrem Push"
+git commit -m "DC 4 Push"
 git push
